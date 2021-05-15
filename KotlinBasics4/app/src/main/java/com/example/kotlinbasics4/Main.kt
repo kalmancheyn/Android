bae5c73dac14b8936bf1ab4a2121a4d321a17bc1
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-05-15 12:53:25 +0200

    .

--- KotlinBasics4/app/src/main/java/com/example/kotlinbasics4/Main.kt
+++ KotlinBasics4/app/src/main/java/com/example/kotlinbasics4/Main.kt
@@ -1,13 +1,35 @@
 package com.example.kotlinbasics4
 
-import com.example.lib.HttpRequest
-import com.example.lib.timeOut
+data class Book(val title: String, val price : Int)  {
+    operator fun plus(other: Book): Int{
+        return price + other.price
+    }
+}
+
+operator fun Book.plus(other: Book): Int {
+    return this.price + other.price - 10
+}
+
+operator fun String.times(n: Int): String {
+    var result : String = ""
+    for (index in 1..n) {
+        result += this
+    }
+    return result
+}
 
 fun main() {
-    Calculator().max(1,2)
+    val a: Int = 1
+    val b: Int = 2
 
-    val httpRequest: HttpRequest = HttpRequest()
-    httpRequest.sendRequest()
+    println(a + b)
+    println(a.plus(b))
 
-    timeOut = 90
+    val effectiveJava = Book("EffectiveJava", 200)
+    val cleanCoders = Book("Clean Coders", 300)
+    println(effectiveJava + cleanCoders)
+
+    var s = "something"
+
+    println(s*4)
 }
