10002ce7d87dfa256dfa21171116c4de9c733327
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-05-23 11:06:53 +0200

    .

---
+++ CollectionsDemo/app/src/main/java/com/example/collectionsdemo/MainList.kt
@@ -0,0 +1,11 @@
+package com.example.collectionsdemo
+
+fun main() {
+    val list = mutableListOf(2, 3, 5, 6, 7)
+    list[2] = 700
+
+    list.removeAt(1)
+    list.remove(6)
+
+    println(list.toString())
+}
