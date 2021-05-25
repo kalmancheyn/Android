71ff6460de7952014f5d351b5b52f657688b4a48
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-05-25 19:57:19 +0200

    .

---
+++ MyFirstAndroidApp/app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myfirstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var rand = Random()
+
+        //DRY Do not Repeat Yourself
+        btnTime.setOnClickListener {
+
+            if (etData.text.isNotEmpty()) {
+                var num = rand.nextInt(100)
+                var currentTime = "$num ${etData.text.toString()} ${Date(System.currentTimeMillis()).toString()}"
+                tvData.text = currentTime
+                Toast.makeText(this, currentTime,Toast.LENGTH_LONG).show()
+            }
+            else {
+                 etData.error = "Ez nem lehet üres"
+            }
+        }
+    }
+}
