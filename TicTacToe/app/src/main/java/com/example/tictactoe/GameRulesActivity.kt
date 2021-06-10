af5cd1d1dd434d13dcf7a38f9f34390ad1cf5d14
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-06-10 15:11:15 +0200

    .

--- TicTacToe/app/src/main/java/com/example/tictactoe/GameRulesActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/GameRulesActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_game_rules.*
 
-class GameRules : AppCompatActivity() {
+class GameRulesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_rules)
--- TicTacToe/app/src/main/java/com/example/tictactoe/MenuActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MenuActivity.kt
@@ -3,7 +3,6 @@ package com.example.tictactoe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_menu.*
 import com.example.tictactoe.model.TicTacToeModel
 
@@ -18,7 +17,7 @@ class MenuActivity : AppCompatActivity() {
         }
 
         btnGameRules.setOnClickListener {
-            startActivity(Intent(this, GameRules::class.java))
+            startActivity(Intent(this, GameRulesActivity::class.java))
         }
 
         btnGameExit.setOnClickListener {
